7627a0ec47db5d9fae9250284ee07ad9b81f8434
kotaroburrito <kotaroburrito@gmail.com>
2024-12-08 01:01:28 +0900

    test

--- mahjong_nanikiru/nanikiru.py
+++ mahjong_nanikiru/nanikiru.py
@@ -7,6 +7,7 @@ Created on 2024/12/01
 import streamlit as st
 import requests
 import os
+from supabase import create_client, Client
 
 # Supabaseとの接続情報
 SUPABASE_URL = os.getenv("STREAMLIT_SUPABASE_URL")
@@ -25,27 +26,49 @@ tiles = [
 
 st.image(tiles, caption=["白", "發", "中"], width=80)
 
-# Superbaseからクイズデータを取得
-def fetch_quiz():
-    headers = {
-        "Authorization": f"Bearer{SUPABASE_KEY}", 
-        "Content-Type": "application/json"
-    }
-        
-    # ランダムなクイズデータを1件取得
-    response = 
-    request.get(f"{SUPABASE_URL}/rest/v1/nanikiru?select=*&limit=1&order=id.asc"), 
-    headers=headers)
-return response.json()
-
 # アプリケーションのメイン
 st.title("Nanikiru?")
 
-# クイズデータを取得
-quiz = fetch_quiz()[0]
+# Superbaseからクイズデータを取得
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# クイズ情報を表示
+def fetch_quiz():
+    response = supabase.table("nanikiru").select("*").execute()
+    if response.status_code == 200 and response.data:
+        # ランダムに1問選ぶ
+        quiz_data = random.choice(response.data)
+        return quiz_data
+    else: 
+        st.error("クイズデータの取得に失敗しました。")
+        return None
 
+# クイズデータのオブジェクト生成
+quiz = fetch_quiz()
+if not quiz: 
+    st.stop()
+
+# 自風、場風、ドラを表示
+st.write(f"自風: {quiz['your_wind']}/ 場風: {quiz['table_wind']}")
+st.write(f"ドラ: {quiz['dragon_normal']}")
+
+# 手牌を表示
+st.write("手牌: ")
+hand_tiles = quiz["hand"].split(",") # 1カラム1牌にするならここは変える!
+for tile in hand_tiles: 
+    tile_url = f"https://raw.githubusercontent.com/kotaroburrito/mahjong/master/images/{quiz['tile']}.PNG"
+    st.image(title_url, width=50)
+
+# ツモを表示
+st.write("ツモ: ")
+zimo_tile_url = f"https://raw.githubusercontent.com/kotaroburrito/mahjong/master/images/{quiz['zimo']}.PNG"
+st.image(zimo_tile_url, width=50)
+
+# 答えを非表示にしておき、回答後に表示
+if st.button("答えを見る"): 
+    correct_tile_url = f"https://raw.githubusercontent.com/kotaroburrito/mahjong/master/images/{quiz['correct_tile']}.PNG"
+    st.write("正解: ")
+    st.image(correct_tile_url, width=50)
+    st.write(f"解説: {quiz['explanation']}")
 
 
 
